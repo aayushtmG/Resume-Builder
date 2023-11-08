de0423c68ecd4c2c1d5aab8d9d32740c92a9bc9a
aayushtmG <aiyoustmg@gmail.com>
2023-11-08 07:53:53 +0545

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Resume-Builder",
+  base:'/Resume-Builder/',
   plugins: [react()],
   server: {
     port: 8000,
