966da275a0d08851e2a5a293d8a2b9541cfab219
aayushtmG <aiyoustmg@gmail.com>
2023-11-08 06:41:46 +0545

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://aayushtmG.github.io/Resume-Builder",
   server: {
     port: 8000,
   },
